0c6c142899d5ab51876a7434602f93300d66844d
polymorvic <polymorphic.bite@gmail.com>
2025-10-15 19:47:43 +0200

    tresting scripts working

---
+++ src/utils/common.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from collections.abc import Hashable as SupportsHash
+
+class Hashable(ABC):
+
+    @abstractmethod
+    def _key_(self) -> SupportsHash:
+        NotImplemented
+
+
+    def __hash__(self) -> int:
+        return hash(self._key_())
+
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self._key_() == other._key_()
